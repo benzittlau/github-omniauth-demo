88d83921ad20a81a0169e1df396746003e163672
Ben Zittlau <ben@zittlau.ca>
2015-11-15 21:12:45 -0700

    Add proper access control to private page.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,13 @@
 class PagesController < ApplicationController
+  before_filter :ensure_logged_in, :only => :private
   def home
   end
 
   def private
+  end
+
+  private
+  def ensure_logged_in
+    redirect_to :root unless logged_in?
   end
 end
